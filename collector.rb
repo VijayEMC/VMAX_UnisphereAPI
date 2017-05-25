d835d0fbece31265364ffcf8aade47998d516933
Kumar <Vijay.Kumar4@emc.com>
2017-05-24 18:33:17 -0700

    first/last change

--- collector.rb
+++ collector.rb
@@ -111,7 +111,7 @@ symIds.each do |sym|
     # Make POST Request
     metrics_object = rest_post(jsonPayload, metrics_url, auth, cert=nil)
     jsonReturn = metrics_object.to_json
-    resultList = jsonReturn['resultlist']['result']
+    resultList = jsonReturn['resultList']['result']
     binding.pry
     ####################################################
     # Organized returned object into influxDB payload
