ddc3f3aeadab393a8bdf8250f44e8a021cbdc533
Kumar <Vijay.Kumar4@emc.com>
2017-09-26 16:39:20 -0700

    adding time specifics into lib

--- collectorLib.py
+++ collectorLib.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import sys
 from influxdb import InfluxDBClient 
 
 #############################################
@@ -23,10 +24,36 @@ class uniInfo:
 
 ##########################################
 # Method: Connect InfluxDB Instance
+# Connects to a running instance of influx
+# Does not seem to allow for error checking
+# NOTE: SHOULD ADD A CONNCECTION CHECK
+# AFTER INFLUXDBCLIENT IS CALLED
 ##########################################
 
 def ccInflux(sysInfo, configObj):
     sysInfo.idb = InfluxDBClient((configObj['influx']['ip'], configObj['influx']['port'], configObj['influx']['user'], configObj['influx']['pass'], configObj['influx']['table']))
+    # Add Error Checking
+
+########################################################
+# NOTES: The next two functions will deal with possible
+# time discrepancies found in the event an application
+# run did not successfully write to Influx. In this
+# event, the app must check the last time a successful
+# write occured, and fill in any missing history by
+# writing a history of points to Influx. Make sense?
+##########################################################
+
+def influxTimeQuery(idbInstance):
+    lastWriteObj = idbInstance.query('select value from cpu_load_short ORDER BY time DESC LIMIT 1;')
+    return lastWriteObj['time']
+
+def _timeDiff(timestamp, influxLastTime):
+    if timestamp - influxLastTime > 6: # 6 minutes? Questionable move
+        return influxLastTime
+    else:
+        return timestamp
+    
+    
     
 ##############################################
 # Method: Get Keys
@@ -38,8 +65,13 @@ def ccInflux(sysInfo, configObj):
 ###############################################
 
 def getKeys(symInfo):
-    return requests.get(symInfo.keysUrl, headers=symInfo.headers, auth=symInfo.auth, verify=False)
-
+    resp = requests.get(symInfo.keysUrl, headers=symInfo.headers, auth=symInfo.auth, verify=False)
+    if resp.status_code < 300:
+        return resp
+    else:
+        print >> sys.stderr, 'Failed in getKeys() with response code' + resp.status_code
+        print('Failed in getKeys() with response code' + resp.status_code)
+        
 ###################################################
 # Method: Process Keys Object
 ###################################################
@@ -58,7 +90,12 @@ def procKeys(arrayInfo, keysObject):
 def collectMetrics(symInfo, index):
     postObject = {'startDate' : symInfo['lastAvail'][index], 'endDate' : symInfo['lastAvail'][index], 'symmetrixId' : symInfo['symIds'][index], 'dataFormat' : 'Average', 'metrics' : config['metrics']}
     jsonPO = json.loads(postObject)
-    return requests.post(symInfo.metricsUrl, jsonPO, headers=symInfo.headers, auth=symInfo.auth, verify=False)
+    resp = requests.post(symInfo.metricsUrl, jsonPO, headers=symInfo.headers, auth=symInfo.auth, verify=False)
+    if resp.status_code < 300:
+        return resp
+    else:
+        print >> sys.stderr, 'Failed in collectMetrics() with response code' + resp.status_code
+        print('Failed in collectMetrics() with response code' + resp.status_code)
 
 ###########################################################
 # Method: Process Metrics Object
@@ -68,6 +105,7 @@ def procMetrics(symInfo, metricsObj, config, index):
     metricList = metricsObj['resultList']['result']
     for i in config['metrics']:
         newValue = metricList[i]
+        # add 'time' : <timestamp>
         influxPayload = {'series': metric, 'values': {'value': newValue}, 'tags': {'symmetrixId': symInfo.symIds[index]}}
         symInfo.influxArray.append(influxPayload)
     symInfo.idb.write_points(symInfo.influxArray)
