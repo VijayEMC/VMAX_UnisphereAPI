d316cfabc6de7a50ee08723b15e9485c388f39a3
Kumar <Vijay.Kumar4@emc.com>
2017-10-30 14:28:13 -0700

    add new collector

--- collector.rb
+++ collector.rb
@@ -7,8 +7,8 @@ require "influxdb"
 
 current_dir=File.dirname(__FILE__)
 new_curr_dir = Dir.pwd
-settings_file=("#{new_curr_dir}/settings.json.example")
-
+settings_file=("#{current_dir}/settings.json")
+#settings_file=("~/grafanaApp/rubyApp/app/settings.json")
 #########################
 # Method: API Post Method
 #########################
@@ -50,64 +50,74 @@ end
 # Method: Read settings.json file
 #################################
 def readSettings(file)
+  #puts(file)
   settings = File.read(file)
   JSON.parse(settings)
 end
 
+
 config=readSettings(settings_file)
 # Create influx instance and connect to InfluxDB
-influxdb = InfluxDB::Client.new config['influx']['table'], host: config['influx']['host'], port: config['influx']['port'], username: config['influx']['username'], password: config['influx']['password']
+influxdb = InfluxDB::Client.new config['influx']['table'], host: config['influx']['ip'], port: config['influx']['port'], username: config['influx']['user'], password: config['influx']['pass']
 
 
-#####################################################
-# Make Keys Call to get SYM IDs and Most Recent Date
-######################################################
 
-# Build our url strings
-keys_url = "https://#{config['unisphere']['ip']}:#{config['unisphere']['port']}/univmax/restapi/performance/Array/keys"
-metrics_url = "https://#{config['unisphere']['ip']}:#{config['unisphere']['port']}/univmax/restapi/performance/Array/metrics" 
-
-# Create base 64 encoded auth
-auth = Base64.strict_encode64("#{config['unisphere']['user']}:#{config['unisphere']['password']}")
-
-# Make call to get keys
-keys_object = rest_get(keys_url, auth, cert=nil)
-
-###################################################
-# NOTE: ADD ERROR CHECKING HERE. ENSURE KEYS_OBJECT
-# IS A VALID RETURN
-####################################################
-#################################################
-# Build POST Request Body Object from Keys Return
-##################################################
-
-# Create array to hold symmetrix IDS and another to hold their last available date
 symIds = []
 lastAvail = []
 firstAvail = []
-
-keys_object['arrayInfo'].each do |arrayObj|
-    symIds << arrayObj['symmetrixId']
-    lastAvail << arrayObj['lastAvailableDate']
-    firstAvail << arrayObj['firstAvailableDate']
-end
-
-# create array to send multiple metrics to influx
+noMetrics = []
+yesMetrics = []
 influxArray = []
 
-#Start a loop that makes requests and dumps requested info into influx
-index = 0
-symIds.each do |sym|
-    postObject = {'startDate' => lastAvail[index], 'endDate' => lastAvail[index], 'symmetrixId' => sym, 'dataFormat' => 'Average', 'metrics' => config['metrics']}
-    # convert object to JSON
-    jsonPayload = postObject.to_json
-    # Make POST Request
-    metrics_object = rest_post(jsonPayload, metrics_url, auth, cert=nil)
+unisphereArray = config['unisphere']
+###########################################
+# Unisphere Instance Loop 
+# Iterate through each Unisphere Instance
+###########################################
+unisphereArray.each do |uniInst|
+        
+    # Build our url strings
+    keys_url = "https://#{uniInst['ip']}:#{uniInst['port']}/univmax/restapi/performance/Array/keys"
+    metrics_url = "https://#{uniInst['ip']}:#{uniInst['port']}/univmax/restapi/performance/Array/metrics" 
+
+    # Create base 64 encoded auth
+    auth = Base64.strict_encode64("#{uniInst['user']}:#{uniInst['password']}")
+ 
+    #####################################################
+    # Make Keys Call to get SYM IDs and Most Recent Date
+    ######################################################
+
+    keys_object = rest_get(keys_url, auth, cert=nil)
+
+    ###################################################
+    # NOTE: ADD ERROR CHECKING HERE. ENSURE KEYS_OBJECT
+    # IS A VALID RETURN
+    ####################################################
+    #################################################
+    # Build POST Request Body Object from Keys Return
+    ##################################################
+
+    # Create array to hold symmetrix IDS and another to hold their last available date
+    keys_object['arrayInfo'].each do |arrayObj|
+            symIds << arrayObj['symmetrixId']
+            lastAvail << arrayObj['lastAvailableDate']
+            firstAvail << arrayObj['firstAvailableDate']
+    end
+    
+    #Start a loop that makes requests and dumps requested info into influx
+    index = 0
+
+    symIds.each do |sym|
+        postObject = {'startDate' => lastAvail[index], 'endDate' => lastAvail[index], 'symmetrixId' => sym, 'dataFormat' => 'Average', 'metrics' => config['metrics']}
+        # convert object to JSON
+        jsonPayload = postObject.to_json
+        # Make POST Request
+        metrics_object = rest_post(jsonPayload, metrics_url, auth, cert=nil)
     #######################################################
     # NOTE: ADD ERROR CHECKING HERE. ENSURE METRICS_OBJECT
     # IS A VALID RETURN
     #######################################################
-    
+
     ###################################################################
     # BEGIN ERROR CHECKING HERE
     #####################################################################
@@ -116,54 +126,64 @@ symIds.each do |sym|
     # DEPRECATED METRIC WE CAN ADD THE PROPER ERROR CHECKING FOR RETURN THAT
     # HAS NO METRICS
     ##########################################################################
-    # create array of sym IDS that did not generate metrics
-    noMetrics = []
-    yesMetrics = []
-    if metrics_object['resultList']['result'] == nil
-        noMetrics << sym 
-    else
-        yesMetrics << sym
-    end
         
-        
-    
-    # move to next sym if sym ID generated no metrics
-    next if metrics_object['resultList']['result'] == nil
-    
+        # fill reporting arrays to print which arrays found metrics
+        if metrics_object['resultList']['result'] == nil
+            noMetrics << sym 
+        else
+            yesMetrics << sym
+        end
+
+
+
+        # move to next sym if sym ID generated no metrics
+        next if metrics_object['resultList']['result'] == nil
+
     #####################################################################
     # END ERROR CHECKING
     ####################################################################
-    
-    #grab relevant data from http response
-    metricList = metrics_object['resultList']['result']
+
+        #grab relevant data from http response
+        metricList = metrics_object['resultList']['result']
 
     ####################################################
     # Organized returned object into influxDB payload
     #####################################################
-    # collect the data from each metric returned from API
-    config['metrics'].each do |metric|
-        # get actual value
-        newValue = metricList[0][metric]
-        # create influx payload
-        influxPayload = {series: metric, values: {value: newValue}, tags: {symmetrixId: sym}}
-        # push the current metric to the array
-        influxArray.push(influxPayload)
+        # collect the data from each metric returned from API
+        config['metrics'].each do |metric|
+            # get actual value
+            newValue = metricList[0][metric]
+            # create influx payload
+            influxPayload = {series: metric, values: {value: newValue}, tags: {symmetrixId: sym}}
+            # push the current metric to the array
+            influxArray.push(influxPayload)
+        end
+        # send array of data points to influx
+        influxdb.write_points(influxArray)
+        # clear array
+        influxArray.clear
+        # increment and loop
+        index += 1
+        
+       
+        
     end
-    # send array of data points to influx
-    influxdb.write_points(influxArray)
-    # clear array
-    influxArray.clear
-    # increment and loop
-    index += 1
-end
-
-# Print results of symmetrix metrics
-# NOTE: ADD ACTUAL METRICS TO RUBY PRINTOUT
-if noMetrics.length >0
-    puts "The following Symmetrix produced NO metrics: "
-    puts noMetrics
-end
-if yesMetrics.length >0
-    puts "The following Symmetrix produced metrics "
-    puts yesMetrics  
+    # Print results of symmetrix metrics
+    # NOTE: ADD ACTUAL METRICS TO RUBY PRINTOUT
+    if noMetrics.length >0
+        puts "The following Symmetrix produced NO metrics: "
+        puts noMetrics
+    end
+    if yesMetrics.length >0
+            puts "The following Symmetrix produced metrics "
+        puts yesMetrics  
+    end
+    
+    # Clear all arrays
+    symIds.clear
+    lastAvail.clear        
+    firstAvail.clear
+    noMetrics.clear
+    yesMetrics.clear
+    
 end
